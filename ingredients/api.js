43e8d9a297ae2572a96e54c67988fef9ecdae266
Angel Ivanov <angel.iwanov@gmail.com>
2024-04-10 06:59:32 +0300

    ingredients

--- ingredients/api.js
+++ ingredients/api.js
@@ -12,26 +12,45 @@ router.get("/client.js", (_, res) =>
  * Student code starts here
  */
 
-// connect to postgres
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
+});
 
 router.get("/type", async (req, res) => {
   const { type } = req.query;
   console.log("get ingredients", type);
 
-  // return all ingredients of a type
+  const { rows } = await pool.query(`SELECT * FROM ingredients WHERE type=$1`, [
+    type,
+  ]);
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  res.status(200).json({ rows });
 });
 
 router.get("/search", async (req, res) => {
+  console.log(req.query);
   let { term, page } = req.query;
   page = page ? page : 0;
   console.log("search ingredients", term, page);
 
-  // return all columns as well as the count of all rows as total_count
-  // make sure to account for pagination and only return 5 rows at a time
+  let whereClause = "";
+  const params = [page * 5];
 
-  res.status(501).json({ status: "not implemented", rows: [] });
+  if (term) {
+    whereClause = `WHERE CONCAT(title, type) ILIKE $2`;
+    params.push(`%${term}%`);
+  }
+  const { rows } = await pool.query(
+    `SELECT *, COUNT(*) OVER ()::INT AS total_count FROM ingredients ${whereClause} OFFSET $1 LIMIT 5`,
+    params
+  );
+
+  res.status(200).json({ rows });
 });
 
 /**
